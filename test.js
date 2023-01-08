d5d3881f3f2f02740f4963e295ce4a7d576cf491
Unknown User <jumperkylian@outlook.com>
2023-01-08 20:17:17 +0100

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const translate = require('popitatranslate')
-
-translate('fr', 'en', 'Bonjour, comment vas-tu ?', (error, translatedText) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(translatedText);
-    }
-  });
